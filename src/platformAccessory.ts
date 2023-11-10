7414752373b58228a62ab56d2a53afe83b6be156
Jon Stroud <jon@strouds.org>
2023-11-10 10:54:02 -0600

    add background polling of the sensor once a minute
    print a log messages if the value ever differs by 2% or more

--- src/platformAccessory.ts
+++ src/platformAccessory.ts
@@ -3,6 +3,7 @@ import { Service, PlatformAccessory, CharacteristicValue } from 'homebridge';
 
 import { ExampleHomebridgePlatform } from './platform';
 import { apiType } from './platform';
+import { setFlagsFromString } from 'v8';
 
 /**
  * Platform Accessory
@@ -21,6 +22,9 @@ export class ExamplePlatformAccessory {
     Brightness: 100,
   };
 
+  private currentLevel;
+  private lastLevel;
+
   constructor(
     private readonly platform: ExampleHomebridgePlatform,
     private readonly accessory: PlatformAccessory,
@@ -36,6 +40,8 @@ export class ExamplePlatformAccessory {
     // get the LightBulb service if it exists, otherwise create a new LightBulb service
     // you can create multiple services for each accessory
     this.service = this.accessory.getService(this.platform.Service.HumiditySensor) || this.accessory.addService(this.platform.Service.HumiditySensor);
+    this.currentLevel = 0;
+    this.lastLevel = 0;
 
     // set the service name, this is what is displayed as the default name on the Home app
     // in this example we are using the name we stored in the `accessory.context` in the `discoverDevices` method.
@@ -47,6 +53,21 @@ export class ExamplePlatformAccessory {
     // register handlers for the On/Off Characteristic
     this.service.getCharacteristic(this.platform.Characteristic.CurrentRelativeHumidity)
       .onGet(this.getLevel.bind(this));               // GET - bind to the `getLevel` method below
+
+    /**
+     * Updating characteristics values asynchronously.
+     *
+     * Example showing how to update the state of a Characteristic asynchronously instead
+     * of using the `on('get')` handlers.
+     * Here we change update the motion sensor trigger states on and off every 10 seconds
+     * the `updateCharacteristic` method.
+     *
+     */
+    this.pollLevel();
+    setInterval(() => {
+      // EXAMPLE - inverse the trigger
+      this.pollLevel();
+    }, 60000);
   }
 
   /**
@@ -62,8 +83,8 @@ export class ExamplePlatformAccessory {
    * @example
    * this.service.updateCharacteristic(this.platform.Characteristic.On, true)
    */
-  async getLevel(): Promise<CharacteristicValue> {
-    let currentValue = 0;
+  async pollLevel() {
+    let newValue = 0;
 
     if (this.ptApi === apiType.localApi) {
       const { data } = await axios.get('http://' + this.accessory.context.device.localIp + 'get_senors', {});
@@ -71,14 +92,28 @@ export class ExamplePlatformAccessory {
       const zero = sensor_data[0].z;
       const ad = sensor_data[0]['1'];
 
-      currentValue = (ad - zero) * this.accessory.context.device.calFactor;
+      newValue = (ad - zero) * this.accessory.context.device.calFactor;
     } else if (this.ptApi === apiType.publicApi) {
       const { data } = await axios.get('https://www.mypt.in/device/' + this.accessory.context.device.cameraId, {});
-      currentValue = data.percentLevel;
+      newValue = data.percentLevel;
     }
-    this.platform.log.debug(this.accessory.context.device.exampleDisplayName, currentValue);
 
-    return currentValue;
+    // push the new value to HomeKit
+    if (newValue != this.lastLevel) {
+      this.platform.log.debug('dbg: update', this.accessory.context.device.exampleDisplayName, newValue);
+      this.service.updateCharacteristic(this.platform.Characteristic.CurrentRelativeHumidity, newValue);
+    }
+
+    const delta = this.currentLevel - newValue;
+    if (delta < -1 || delta > 1) {
+      this.platform.log.info(this.accessory.context.device.exampleDisplayName, newValue);
+      this.currentLevel = newValue;
+    }
+    this.lastLevel = newValue;
+  }
+
+  async getLevel(): Promise<CharacteristicValue> {
+    return this.currentLevel;
   }
 
 
